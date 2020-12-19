e4292541cc9e466cfdb0254abc865c0414d32b22
kevin <kevin.santander@protonmail.ch>
2020-12-19 14:32:00 +0100

    commit main

---
+++ notreunivers.php
@@ -0,0 +1,37 @@
+<?php 
+    include "common/menu.php";
+?>
+<div class="container-fluid">
+  <div class="row-cards-univers">
+    <div>
+      <div>
+      <img class="fit-picture"
+          src="img/card/A_demi_mot.png"
+          alt="A-demi-mots-paroles">
+      </div>
+    </div> 
+    <div>
+      <div>
+      <img class="fit-picture"
+          src="img/card/A_demi_mot.png"
+          alt="A-demi-mots-paroles">
+      </div>
+    </div> 
+  </div>
+  <div class="">
+  <div>
+      <div>
+      <img class="fit-picture"
+          src="img/card/A_demi_mot.png"
+          alt="A-demi-mots-paroles">
+      </div>
+    </div> 
+    <div>
+      <div>
+      <img class="fit-picture"
+          src="img/card/A_demi_mot.png"
+          alt="A-demi-mots-paroles">
+      </div>
+    </div> 
+  </div>
+</div>
