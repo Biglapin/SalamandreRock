27fcbed6a11976777c04641156bcf6765b00b646
kevin SANTANDER <kevin.santander@protonmail.ch>
2021-03-04 21:55:38 +0100

    création premier album avec accordion

---
+++ notre_univers.php
@@ -0,0 +1,79 @@
+<?php 
+   include "common/menu.php";
+   ?>
+<body>
+<hr>
+   <header class="row row-title-top">
+      <h1 class="title-top">Lyrics</h1>
+   </header>
+   
+   <section class="row row-album">
+     <aside class="col-left-album col-7">
+       <img src="img/notre_univers/Pochette_album_1.jpg" alt="Pochete album 1 de Salamandre Rock">
+     </aside>
+     
+     <aside class="col-right-album col-4">
+        <h1>LE JOUR D'APRÈS</h1>
+        <h2>Description de l'album</h2>
+        <h3 class="date">2017</h3>
+           <a class="social-button spotify" href="https://open.spotify.com/album/4X9gJqMUcPLk64hNQwU07d?si=kfIjNV6OQqGz6YuQoDwD0g">
+           <i class="fa fa-spotify" aria-hidden="true"></i>
+           </i>
+  </a>
+        <section class="Accordion">
+  <ul class="Accordion__tabs">
+    <li class="Accordion__tab" onclick="toggleAccordion(this)">
+      <div class="Accordion__tab__headline">
+        <h4>Souviens toi</h4><span class="icon"></span>
+      </div>
+      <div class="Accordion__tab__content">
+        <div class="wrapper">
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+          </p>
+        </div>
+      </div>
+    </li>
+    <li class="Accordion__tab" onclick="toggleAccordion(this)">
+      <div class="Accordion__tab__headline">
+        <h4>Le jour d'après'</h4><span class="icon"></span>
+      </div>
+      <div class="Accordion__tab__content">
+        <div class="wrapper">
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
+consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+    </li>
+    <li class="Accordion__tab" onclick="toggleAccordion(this)">
+      <div class="Accordion__tab__headline">
+        <h4>Beauté du mal</h4><span class="icon"></span>
+      </div>
+      <div class="Accordion__tab__content">
+        <div class="wrapper">
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </div>
+    </li>
+    <li class="Accordion__tab" onclick="toggleAccordion(this)">
+      <div class="Accordion__tab__headline">
+        <h4>Caméléon</h4><span class="icon"></span>
+      </div>
+      <div class="Accordion__tab__content">
+        <div class="wrapper">
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+          </p>
+        </div>
+      </div>
+    </li>
+  </ul>
+</section>
+    
+
+   </section>
+   </aside>
+   <script src="js/notre_univers.js"></script>
+</body>
