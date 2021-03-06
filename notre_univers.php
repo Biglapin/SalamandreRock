f46438f42741aeb8e92aa453b255fec130ca99ee
kevin SANTANDER <kevin.santander@protonmail.ch>
2021-03-06 16:40:59 +0100

    finalisation test page notre univers

--- notre_univers.php
+++ notre_univers.php
@@ -4,15 +4,29 @@
 <body>
 <hr>
    <header class="row row-title-top">
-      <h1 class="title-top">Lyrics</h1>
+   <h4 class="text-univers">Comme un fil, une toile que l'on tisse entre réel et imaginaire<br>
+      Les pensées que l'on traîne derrière soi indélébiles ou éphémères.<br>
+      Des maux, Des notes en bandoulière, Des états d'âmes à coeur ouvert.<br>
+      Comme un souffle nouveau, un courant d'air remplit d'espoir<br>
+      Où se mélangent tristesse, joie, avec pour fond l'envie de croire.<br>
+      C'est comme une rage au corps instinctive, à fleur de peau.<br>
+      Qui nous pousse à écrire à donner un sens aux mots.<br>
+      Comme un souffle, une envie de traverser de l'autre côté.<br>
+      Effacer son reflet, un miroir sans beauté.<br>
+      Comme une mélodie, un hymne à notre résistance<br>
+      Des étoiles plein la tete à la place des silences<br>
+      Comme un certain regard que l'on porte sur le temps<br>
+      En comprenant le passé pour conjuguer le présent
+    </h4>
+    <!--  <h1 class="title-top">Lyrics</h1> -->
    </header>
    
    <section class="row row-album">
-     <aside class="col-left-album col-7">
+     <aside class="col-left-album col-lg-7 col-sm-12">
        <img src="img/notre_univers/Pochette_album_1.jpg" alt="Pochete album 1 de Salamandre Rock">
      </aside>
      
-     <aside class="col-right-album col-4">
+     <aside class="col-right-album col-lg-4 col-sm-12 ">
         <h1>LE JOUR D'APRÈS</h1>
         <h2>Description de l'album</h2>
         <h3 class="date">2017</h3>
@@ -24,11 +38,47 @@
   <ul class="Accordion__tabs">
     <li class="Accordion__tab" onclick="toggleAccordion(this)">
       <div class="Accordion__tab__headline">
-        <h4>Souviens toi</h4><span class="icon"></span>
+        <h4>Passager</h4><span class="icon"></span>
       </div>
       <div class="Accordion__tab__content">
         <div class="wrapper">
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+          <p>Je suis ton cœur<br>
+            Tu es ma voix<br>
+            Un rire entremêlé de joie<br>
+            Une défense, une esquive<br>
+            Un regard maladroit<br>
+            Tu es un tout<br>
+            Tu es ma vie<br>
+            Mon oxygène<br>
+            Mon ennui<br>
+            Des fourmis au bout des doigts<br>
+            A crever les silences on finit encore<br>
+            A déchirer le ciel et à éclabousser le nord<br>
+            Des passants dépassés<br>
+            A la santé de nos filles<br>
+            Osons à présent, ose Éva<br>
+            A vouloir t'accrocher à ...v
+            Refrain :<br><br>
+            Time time time time<br>
+            Time is running out x4<br><br>
+            A Rita, A Nino<br>
+            A la mémoire des anciens<br>
+            De ce que nous avons appris de vous<br>
+            A la croisée des chemins<br>
+            Orphelins de vos rires<br>
+            Habitués aux silences<br>
+            S'élancent, s'élancent, immense, immense<br>
+            Un rêve de gosses , des jeux d'enfants<br>
+            des passagers clandestins,<br>
+            Clairsemés, pas tentés<br>
+            à vivre l'incertain.<br>
+            Incertitude dans vos gestes<br>
+            Incertitude dans la voix<br>
+            Osons à présent, Osons Eva à nous<br>
+            Accrocher à ..<br>
+            Refrain :<br><br>
+            Time time time time<br>
+            Time is running out x4 <br>
           </p>
         </div>
       </div>
