858981713b310084e287221a92c53837d870c71a
kevin SANTANDER <kevin.santander@protonmail.ch>
2021-03-27 21:03:41 +0100

    fusion

--- common/menu.php
+++ common/menu.php
@@ -1,52 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <!-- FONT -->
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
-    </style>
-
-        <title>Salamandre Rock</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Site officiel du groupe Salamandre">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/notre_univers.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    </head>
-
-    <header>
-            <nav class="navbar navbar-expand-lg black navbar-dark fixed-top">
-                <div class="col-lg-1"></div>
-                <a class="navbar-brand col-sm-9 col-lg-2" href="#"><h2><span class="yellow">S</span>alamandre <span class="yellow">R</span>ock</h2></a>
-                <button
-                    type="button"
-                    class="navbar-toggler col-sm-1"
-                    data-toggle="collapse"
-                    data-target="#navbardropdown"
-                    aria-controls="navbardropdown"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2vh" height="2vh" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                    </svg>
-                </button>
-               <div class="col-lg-2"></div> 
-                <div class="collapse navbar-collapse" id="navbardropdown"id="navbar-1">
-                    <ul class="nav nav-pills navbar-nav mr-auto">
-                        <li class="nav-item"><a class="nav-link shape2" href="#about">Intro</a></li>
-                        <li class="nav-item"><a class="nav-link shape2" href="#incomming">A venir</a></li>
-                        <li class="nav-item"><a class="nav-link" href="notre_univers.php">Notre univers</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#light">En lumière</a></li>
-                    </ul>
-                </div> 
-            </nav> 
-        </header>
+ <!DOCTYPE html>
+ <html>
+ <header>
+    <nav class="navbar navbar-expand-lg black navbar-dark fixed-top">
+        <div class="col-lg-1"></div>
+        <a class="navbar-brand col-sm-9 col-lg-2" href="#"><h2><span class="yellow">S</span>alamandre <span class="yellow">R</span>ock</h2></a>
+        <button
+            type="button"
+            class="navbar-toggler col-sm-1"
+            data-toggle="collapse"
+            data-target="#navbardropdown"
+            aria-controls="navbardropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="2vh" height="2vh" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+            </svg>
+        </button>
+       <div class="col-lg-2"></div> 
+        <div class="collapse navbar-collapse" id="navbardropdown"id="navbar-1">
+            <ul class="nav nav-pills navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link shape2" href="#about">Intro</a></li>
+                <li class="nav-item"><a class="nav-link shape2" href="#incomming">A venir</a></li>
+                <li class="nav-item"><a class="nav-link" href="notre_univers.php">Notre univers</a></li>
+                <li class="nav-item"><a class="nav-link" href="#light">En lumière</a></li>
+            </ul>
+        </div> 
+    </nav> 
+</header>
         
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-        <script src="js/script"></script>
-    </body>
 </html>
