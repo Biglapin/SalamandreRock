9c6aa306db9313c933a532b394374860c2c06a1c
kevin SANTANDER <kevin.santander@protonmail.ch>
2021-04-12 22:30:12 +0200

    ajout des dates et boutons concerts

--- Avenir.php
+++ Avenir.php
@@ -1,47 +1,130 @@
-<?php 
-    include "common/menu.php";
-?>
-<div class="container">
-    <div class="card mb-3 mt-3 border border-dark" style="max-width: 540px; margin-left: 30%;padding: 1px;">
-    <div class="row no-gutters">
-        <div class="col-md-4">
-        <img src="img/card/newshow.jpg" class="card-img" alt="image-new-show">
-        </div>
-        <div class="col-md-8">
-        <div class="card-body dark">
-            <h5 class="card-title">12.12.2020</h5>
-                <p class="card-text">Rock'n Hit (Lyon 69)</p>
-                <p class="card-text"><a href="https://www.rockneat.com/"><img src="img/facebook-black.svg" alt="logo-facebook" width="32" height="32" title="facebook" ></a></p>
-        </div>
-        </div>
-    </div>
-    </div>
-    <div class="card mb-3" style="max-width: 540px; margin-left: 30%;padding: 1px;">
-    <div class="row no-gutters">
-        <div class="col-md-4">
-        <img src="img/card/oldshow.jpg" class="card-img" alt="image-old-show">
-        </div>
-        <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title">10.10.2020</h5>
-            <p class="card-text">Ninkasi (Vienne 38)</p>
-            <p class="card-text"><a href="https://www.ninkasi.fr/lieux/saint-romain-en-gal.html"><img src="img/facebook-black.svg" alt="logo-facebook" width="32" height="32" title="facebook" ></a></p>
-        </div>
-        </div>
-    </div>
-    </div>
-    <div class="card mb-3" style="max-width: 540px;margin-left: 30%;padding: 1px;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="img/card/oldshow.jpg" class="card-img" alt="image-old-show">
-            </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">02.08.2020</h5>
-                <p class="card-text">Au Rock'N'Camp (Goudet 43)</p>
-                <p class="card-text"><a href="http://www.rockncamp.com/"><img src="img/facebook-black.svg" alt="" width="32" height="32" title="facebook" ></a></p>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <!-- FONT -->
+   <style>
+       @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
+   </style>
+       <title>À VENIR - Salamandre Rock</title>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta name="description" content="Site officiel du groupe Salamandre">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" />        <link href='https://fonts.googleapis.com/css?family=Lobster|Raleway:400,200' rel='stylesheet' type='text/css'>
+       <!-- CSS only -->
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">        <link rel="stylesheet" href="css/navbar.css">
+       <link rel="stylesheet" href="css/avenir.css">
+     </head>
+
+<body>
+
+  <!-- NAVBAR -->
+
+  <?php include "common/menu.php"; ?>
+
+  <!-- HR -->
+  <hr>
+
+  <!-- TITLE -->
+  <div class="row row-title-top">
+     <h1 class="title-top">CONCERTS</h1> 
+  </div>
+
+  <!-- BUTTONS DATES -->
+  <div class="row row-concerts">
+    <div class="col-new-concerts">
+       <div class='button'>
+              <div class='line'></div> 
+              <div class='line'></div>
+              <div class='line'></div>
+              <div class='line'></div>
+              <div class='line'></div>
+              <div class='line'></div>
+              <span>UPCOMING DATES</span>
         </div>
     </div>
-</div>
+    <div class="col-old-concerts">
+       <div class='button'>
+           <div class='line'></div> 
+           <div class='line'></div>
+           <div class='line'></div>
+           <div class='line'></div>
+           <div class='line'></div>
+           <div class='line'></div>
+           <span>PAST DATES</span>
+       </div>
+    </div>
+  </div>
+  <hr>
+
+    <!-- DATES -->
+    <div class="row row-dates">
+       <div class="col-2">
+           <div>FRI 2 NOV</div>
+       </div>
+       <div class="col-2">
+           <div>BARBEROUSSE</div>
+           <div>PARIS, FRANCE</div>
+       </div>
+       <div class="col-2">
+           <button class="btn btn-primary">Facebook</button>
+           <button class="btn btn-primary">Website</button>
+       </div>
+    </div>
+
+    <hr>
+
+    <div class="row row-dates">
+       <div class="col-2">
+           <div>FRI 2 NOV</div>
+       </div>
+       <div class="col-2">
+           <div>BARBEROUSSE</div>
+           <div>PARIS, FRANCE</div>
+       </div>
+       <div class="col-2">
+           <button class="btn btn-primary">Facebook</button>
+           <button class="btn btn-primary">Website</button>
+       </div>
+    </div>
+
+    <hr>
+
+    <div class="row row-dates">
+       <div class="col-2">
+           <div>FRI 2 NOV</div>
+       </div>
+       <div class="col-2">
+           <div>BARBEROUSSE</div>
+           <div>PARIS, FRANCE</div>
+       </div>
+       <div class="col-2">
+           <button class="btn btn-primary">Facebook</button>
+           <button class="btn btn-primary">Website</button>
+       </div>
+    </div>
+
+    <hr>
+    
+    <div class="row row-dates">
+       <div class="col-2">
+           <div>FRI 2 NOV</div>
+       </div>
+       <div class="col-2">
+           <div>BARBEROUSSE</div>
+           <div>PARIS, FRANCE</div>
+       </div>
+       <div class="col-2">
+           <button class="btn btn-primary">Facebook</button>
+           <button class="btn btn-primary">Website</button>
+       </div>
+    </div>
+
+    <!-- SCRIPTS -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- JavaScript Bundle with Popper BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>  
+    <!-- SCRIPT PAGE -->
+    <script src="js/notre_univers.js"></script>         
+</body>
+</html>
